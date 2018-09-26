134e7a1c273dfa0cc5943cc7e171de7b66b25259
morfiy <43200664+mofriy@users.noreply.github.com>
2018-09-26 10:18:20 +0300

    Add files via upload

---
+++ my1stpr.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math   #add modul math
+import numpy  #add modul numpy
+import matplotlib.pyplot as mpp  #add modul matplotlib and rename it to mpp
+
+
+if __name__=='__main__':    #условие
+    arguments = numpy.r_[0:200:0.1]#arguments - множество чисел от 0 до 200 каждые 0.1
+    mpp.plot(  #вызов модуля 
+        arguments,  #???
+        [math.sin(a) * math.sin(a/20.0) for a in arguments]  #для а из arguments считать функцию
+    )  # скобочка))
+    mpp.show()  #показать график
