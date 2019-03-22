8472f12afd57e182e367e11d21fc4a7253467d80
morfiy <bmd.spb@gmail.com>
2019-03-22 20:24:45 +0300

    updated

--- pandas/data_generator.py
+++ pandas/data_generator.py
@@ -1,6 +1,8 @@
-import random
+﻿import random
 #у меня данных с лаб. работ нету, поэтому так
 f = open('data.txt', 'w')
-for i in range(500):
-    j = random.random()*500
-    f.write(str(j) + '\n')
+for i in range(1000):
+    t = 0
+    for k in range(100):
+        t += random.randint(0,1)
+    f.write(str(t) + '\n')
